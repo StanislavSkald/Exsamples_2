ff6393f7dd5aad264b7c2a30590c0f78ff1eecff
Stanislsav <Sculkoskij@gmail.ru>
2023-03-06 12:03:28 +0300

    Задача 13 Отслеживание дпо файла

--- Exsample_2/Program.cs
+++ Exsample_2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.WriteLine ("Введите число" + " ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 100)
+{
+while (number > 1000)
+{
+number /= 10;
+}
+int result = number % 10;
+Console.WriteLine (result);
+}
+else
+{
+    Console.WriteLine ("Третьей цифры нет");
+}
